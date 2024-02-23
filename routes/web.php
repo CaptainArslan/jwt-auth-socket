00efce6f39edaa0adc922a5542d49ab6360d8cc0
Muhammad Arslan <mughalarslan996@gmail.com>
2024-02-23 22:14:48 +0500

    add chat page

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return redirect()->route('login');
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('/chat', function () {
+        return view('chat');
+    })->name('chat');
 
-require __DIR__.'/auth.php';
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
+require __DIR__ . '/auth.php';
